24cc6bc63718f26af4f5c6439d09e85221b8bf5b
Alex Sutton <alexsutton14@hotmail.com>
2019-10-16 16:21:35 +0100

    I4: Added changing background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 NUMBER = rand(100)+1
+color = 
 
+def difference(input)
+    return input - NUMBER
+end
 def check_guess(input_guess)
     if !input_guess || input_guess == ""
         return ""
@@ -23,9 +27,22 @@ def check_guess(input_guess)
         return ""
     end
 end
+def change_color(input_diff)
+    if input_diff > 9 || input_diff < -9
+        return "red"
+    elsif input_diff > 0 || input_diff < 0
+        return "pink"
+    elsif input_diff == 0
+        return "green"
+    else
+        return "white"
+    end
+end
 
 get '/' do
-    guess = params["guess"]
+    guess = params["guess"].to_i
+    diff = difference(guess)
     message = check_guess(guess)
-    erb :index, :locals => {:number => NUMBER, :message => message}
+    color = change_color(diff)
+    erb :index, :locals => {:number => NUMBER, :message => message, :color => color}
 end
